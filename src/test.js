131824d3fbad2120801ebf3d7235701529ce7f3e
BA89GN1 <weiy63@mail2.sysu.edu.cn>
2026-03-18 04:05:20 +0800

    remove unnecessary files

--- src/test.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const iohook = require(path.join(__dirname, '../node_modules/iohook'));
-
-iohook.on('mousedown', event => {
-  console.log('Mouse down:', event);
-});
-
-iohook.start();
